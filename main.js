3746adeb1dd3d00aadcb9c782f6e0fbbb5b7c593
Antoine Jean <antoinejeanmec@hotmail.fr>
2018-12-15 20:05:55 +0100

    yoyo

--- main.js
+++ main.js
@@ -6,13 +6,13 @@
     $('.navbar-collapse').collapse('hide');
   });
 
-  // Activate scrollspy to add active class to navbar items on scroll
+
   $('body').scrollspy({
     target: '#mainNav',
     offset: 54
   });
 
-  // Collapse Navbar
+
   var navbarCollapse = function () {
     if ($("#mainNav").offset().top > 100) {
       $("#mainNav").addClass("navbar-shrink");
@@ -20,15 +20,11 @@
       $("#mainNav").removeClass("navbar-shrink");
     }
   };
-  // Collapse now if page is not at top
+
   navbarCollapse();
 
-  // Collapse the navbar when page is scrolled
+
   $(window).scroll(navbarCollapse);
-
-
-
-
 
 
 })(jQuery);
